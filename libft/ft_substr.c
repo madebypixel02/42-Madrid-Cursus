4dde0283059be3a27a6396b430767e60bf709c0f
mdebypixel02 <madebypixel02@gmail.com>
2021-04-08 23:44:26 +0200

    Small fixes to guarantee that all cases respond the way they should

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 16:53:56 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/07 10:28:46 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/04/08 18:52:48 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	i = 0;
 	substr = (char *)malloc(len + 1);
+	if (substr == NULL)
+		return (NULL);
 	while (i != len)
 	{
 		substr[i] = s[i + start];
