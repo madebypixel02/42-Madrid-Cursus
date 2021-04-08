1fb1bcfd0917f6795de9c5e541e2767cc17d2cdb
mdebypixel02 <madebypixel02@gmail.com>
2021-04-08 23:46:24 +0200

    ft_putnbr_fd can now handle the min int and zero properly (similar solution to C 00 ex07 from the Piscine)

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 17:03:16 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/07 19:46:29 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/04/08 17:38:37 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,23 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int		digit;
-	char	c;
-
 	if (n == -2147483648)
 	{
 		ft_putchar_fd('-', fd);
 		ft_putchar_fd('2', fd);
 		ft_putnbr_fd(147483648, fd);
+		return ;
+	}
+	else if (n > 9 && n <= 2147483647)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 	else if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
 		ft_putnbr_fd(-n, fd);
 	}
-	else if (n > 0 && n <= 2147483647)
-	{
-		digit = n % 10;
-		c = digit + '0';
-		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd(c, fd);
-	}
+	else
+		ft_putchar_fd(n + '0', fd);
 }
