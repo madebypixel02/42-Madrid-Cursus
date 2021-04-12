9c516fbb9b3bc3b328114398afa5e809d9a3ca9a
mdebypixel02 <madebypixel02@gmail.com>
2021-04-12 20:42:39 +0200

    Minor tweaks

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 16:59:00 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/12 14:41:21 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/04/12 20:27:47 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 	len = ft_strlen(s1);
 	i = 0;
 	j = len - 1;
-	while (ft_strchr(set, s1[i]) != NULL && i < len / 2)
+	while (ft_strchr(set, s1[i]) != NULL && i <= len / 2)
 		i++;
-	while (ft_strchr(set, s1[j]) != NULL && j > len / 2)
+	while (ft_strchr(set, s1[j]) != NULL && j >= len / 2)
 		j--;
 	if (i >= j)
 	{
 		trimmed = (char *)malloc(1);
+		if (!trimmed)
+			return (0);
 		trimmed[0] = '\0';
 		return (trimmed);
 	}
