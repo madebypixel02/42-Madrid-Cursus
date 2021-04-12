2b1baa0b24392e74d8936906e89433494e22fa7e
Alejandro Perez bueno <aperez-b@c3r1s2.42madrid.com>
2021-04-12 14:50:23 +0200

    Probably fixed abort!

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 16:59:00 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/12 14:21:48 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/04/12 14:41:21 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,26 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int	len;
-	int	i;
-	int	j;
+	int		len;
+	int		i;
+	int		j;
+	char	*trimmed;
 
 	if (!s1)
 		return (0);
 	len = ft_strlen(s1);
 	i = 0;
 	j = len - 1;
-	while (ft_strchr(set, s1[i]) != NULL)
+	while (ft_strchr(set, s1[i]) != NULL && i < len / 2)
 		i++;
-	while (ft_strchr(set, s1[j]) != NULL)
+	while (ft_strchr(set, s1[j]) != NULL && j > len / 2)
 		j--;
 	if (i >= j)
-		len = 0;
+	{
+		trimmed = (char *)malloc(1);
+		trimmed[0] = '\0';
+		return (trimmed);
+	}
 	else
 		len = j - i;
 	return (ft_substr(s1, i, len + 1));
