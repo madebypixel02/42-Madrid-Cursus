515398af82199d19df7c632ca97f7a04b923ece6
Alejandro Perez bueno <aperez-b@c3r1s2.42madrid.com>
2021-04-13 20:25:21 +0200

    First attempt at lst-related functions

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 09:15:29 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/13 10:10:34 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/04/13 20:20:20 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,17 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*newlst;
+
+	newlst = (t_list *)malloc(ft_lstsize(lst));
+	newlst = lst;
+	while (newlst)
+	{
+		if (!newlst->content)
+			del(newlst->content);
+		newlst->content = f(newlst->content);
+		newlst = newlst->next;
+	}
+	free(newlst);
+	return (lst);
 }
