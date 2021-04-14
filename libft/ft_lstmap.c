5cb70355c4cfd594ef3ba2c9419b62347baf4ffe
mdebypixel02 <madebypixel02@gmail.com>
2021-04-14 17:14:49 +0200

    Added recursive function to apply function without losing the first node of the list

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,24 +6,31 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 09:15:29 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/14 14:06:23 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/04/14 17:13:55 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+t_list	*ft_lstapply(t_list *lst, t_list *start, void *(*f)(void *), void (*del)(void *))
+{
+	if (lst)
+	{
+		start = ft_lstnew(f(lst->content));
+		if (!start)
+		{
+			del(start->content);
+			free(start->content);
+		}
+		start->next = ft_lstapply(lst->next, start->next, f, del);
+	}
+	return (start);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*head;
-	t_list	*tail;
+	t_list	*start;
 
-	head = ft_lstnew(f(lst->content));
-	tail = head;
-	while (tail)
-	{
-		tail = ft_lstnew(f(lst->content));
-		lst = lst->next;
-		tail = tail->next;
-	}
-	return (head);
+	start = ft_lstapply(lst, start, f, del);
+	return (start);
 }
