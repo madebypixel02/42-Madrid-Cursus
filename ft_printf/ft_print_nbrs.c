bf193aff6b5ca790800c8eeb1423f20ac7e2ff4b
madebypixel02 <madebypixel02@gmail.com>
2021-04-29 11:39:39 +0200

    Small compilation fixes

--- ft_printf/ft_print_nbrs.c
+++ ft_printf/ft_print_nbrs.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 09:58:43 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/28 14:29:19 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/04/29 11:34:25 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int	ft_print_d_i(t_format f, va_list ap)
     count +=  write(1, nbr, len);  
     if (f.minus != -1 && abs(f.width) > f.precision)
         count += ft_putnchar_fd(' ', 1, f.width - f.precision - sign);*/
-	if ((f.precision < len || f.precision <= 0\
+	if (((f.precision < len || f.precision <= 0)\
 		&& *nbr != '0') || (*nbr == '0' && !f.precision_specified))
 		f.precision = len;
 	if (!f.precision_specified)
