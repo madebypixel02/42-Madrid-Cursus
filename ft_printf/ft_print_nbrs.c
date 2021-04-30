f3d29c59072c2834e07b3025eae1d9048747f278
madebypixel02 <madebypixel02@gmail.com>
2021-04-30 13:40:15 +0200

    Sometimes extra zeroes or spaces appear

--- ft_printf/ft_print_nbrs.c
+++ ft_printf/ft_print_nbrs.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 09:58:43 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/29 18:52:59 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/04/30 12:47:46 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,35 +24,28 @@ int	ft_print_d_i(t_format f, va_list ap)
 	int		count;
 	int		len;
 	int		sign;
-	int		n;
 
 	count = 0;
-	n = va_arg(ap, int);
-	nbr = ft_itoa(n);
+	nbr = ft_itoa(va_arg(ap, int));
 	sign = *nbr == '-';
 	len = ft_strlen(nbr);
-	if (n == 0 && f.precision <= 0 && f.precision_specified)
-        len = 0;
-    if ((f.precision < 0 || f.precision < len))
-        f.precision = len;
-    if (sign && f.zero && ((f.precision_specified && f.precision < 0) || (!f.precision_specified && sign)))
-        count += ft_putchar_fd('-', 1);
-    if (!f.minus && f.width > f.precision && f.zero)
-    {
-        if ((f.precision_specified && f.precision < 0) || (!f.precision_specified && sign))
-            count += ft_putnchar_fd('0', 1, f.width - f.precision - sign);
-    }
-    //UNIR ESTAS DOS CONDICIONES CON UN ELSE
-	if (f.minus && f.width > f.precision)
-		count += ft_putnchar_fd(' ', 1, f.width - f.precision - sign);
-	if (sign)
-		count += ft_putchar_fd('-', 1);
-	if (!f.precision_specified  && n == 0)
-		f.precision = f.width;
+	if (*nbr == '0' && f.precision <= 0 && f.precision_specified)
+	{
+		*nbr = '\0';
+		len = 0;
+	}
+	if ((f.precision <= 0 || f.precision < len))
+		f.precision = len;
+	count += ft_putnchar_fd('-', 1, (int)(sign && f.zero));
+	if (!f.minus && f.width > f.precision && !f.precision_specified && f.zero)
+		count += ft_putnchar_fd('0', 1, f.width - f.precision);
+	else if (!f.minus && f.width > f.precision)
+		count += ft_putnchar_fd(' ', 1, f.width - f.precision);
+	count += ft_putnchar_fd('-', 1, (int)(sign && !f.zero));
 	if (f.precision > len)
-		count += ft_putnchar_fd('0', 1, f.precision - len);
-	count +=  write(1, nbr, len);  
-	if (f.minus != -1 && f.width > f.precision)
+		count += ft_putnchar_fd('0', 1, f.precision - len + sign);
+	count +=  ft_putstr_fd(&nbr[sign], 1);
+	if (f.minus && f.width > f.precision)
 		count += ft_putnchar_fd(' ', 1, f.width - f.precision - sign);
 	free(nbr);
 	return (count);
