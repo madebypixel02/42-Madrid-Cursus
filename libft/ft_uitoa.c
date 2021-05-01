08c69a5fd3c07b03f9dd638cd5786ff2c96f0687
Alejandro Perez bueno <aperez-b@c3r1s2.42madrid.com>
2021-05-01 12:04:33 +0200

    Fixed small issue with uitoa

--- libft/ft_uitoa.c
+++ libft/ft_uitoa.c
@@ -6,24 +6,28 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 17:00:03 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/05/01 11:41:14 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/01 12:03:50 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_allocate(char *str, int len, unsigned int n, unsigned int n_bak)
+static char	*ft_allocate(char *str, int len, unsigned int n)
 {
 	str = (char *)malloc(len + 1);
 	if (str == NULL)
 		return (NULL);
 	str[len] = '\0';
-	while (len-- > 0)
+	if (n != 0)
 	{
-		if (len > 0 || n_bak == 0)
+		while (len-- > 0)
+		{
 			str[len] = (n % 10) + '0';
-		n /= 10;
+			n /= 10;
+		}
 	}
+	else
+		str[0] = '0';
 	return (str);
 }
 
@@ -31,12 +35,10 @@ char	*ft_uitoa(unsigned int n)
 {
 	char			*str;
 	int				len;
-	unsigned int	n_bak;
 
-	n_bak = n;
 	len = ft_nbrlen(n, 10);
 	str = NULL;
-	str = ft_allocate(str, len, n, n_bak);
+	str = ft_allocate(str, len, n);
 	if (str == NULL)
 		return (NULL);
 	return (str);
