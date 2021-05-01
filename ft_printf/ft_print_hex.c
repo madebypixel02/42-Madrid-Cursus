c4668230b7af184cbb3594c095f2e6ba9dc4745f
Alejandro Perez bueno <aperez-b@c3r1s2.42madrid.com>
2021-05-01 13:43:03 +0200

    Initial version for %x , %X and %p

--- ft_printf/ft_print_hex.c
+++ ft_printf/ft_print_hex.c
@@ -6,14 +6,57 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 10:26:21 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/05/01 11:37:26 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/01 13:37:11 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	ft_recursive_hex(t_format f, size_t n)
+{
+	int		count;
+	int		remainder;
+	char	character;
+
+	count = 0;
+	if (n > 0)
+	{
+		remainder = n % 16;
+		if (f.specifier != 'X')
+			character = HEXALOW[remainder];
+		else
+			character = HEXAUP[remainder];
+		n /= 16;
+		count += ft_recursive_hex(f, n);
+		count += ft_putchar_fd(character, 1);
+	}
+	return (count);
+}
+
 int	ft_print_hex(t_format f, va_list ap)
 {
-	va_arg(ap, int);
-	return (f.specifier);
+	int		count;
+	size_t	n;
+	int		len;
+
+	count = 0;
+	n = va_arg(ap, size_t);
+	len = ft_nbrlen(n, 16);
+	if (f.specifier == 'p')
+		len += 2;
+	if (n == 0 && f.precision <= 0 && f.precision_specified)
+		len = 0;
+	if (f.precision < 0 || f.precision < len || !f.precision_specified)
+		f.precision = len;
+	count += write(1, "0x", 2 * (f.specifier == 'p' && f.zero));
+	if (!f.minus && f.width > f.precision && !f.precision_specified && f.zero)
+		count += ft_putnchar_fd('0', 1, f.width - f.precision);
+	else if (!f.minus && f.width > f.precision)
+		count += ft_putnchar_fd(' ', 1, f.width - f.precision);
+	count += write(1, "0x", 2 * (f.specifier == 'p' && !f.zero));
+	count += ft_putnchar_fd('0', 1, f.precision - len);
+	count += ft_recursive_hex(f, n);
+	if (f.minus && f.width > f.precision)
+		count += ft_putnchar_fd(' ', 1, f.width - f.precision);
+	return (count);
 }
