10f416f36298d7d2469573f1d8346f096582ceb8
madebypixel02 <madebypixel02@gmail.com>
2021-05-04 11:41:53 +0200

    Added leak tester (fixed segmentation fault)

--- ft_printf/tests/main.c
+++ ft_printf/tests/main.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 09:37:37 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/05/04 11:37:19 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/04 11:41:32 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@
 /* ******** Leaks ******** */
 void	check_leaks(int argc, char *uname)
 {
-	if (!ft_strncmp(uname, "Linux", 1) && argc == 2)
+	if (!ft_strncmp(uname, "Linux", 1))
 		system("valgrind -q --leak-check=full ./a.out");
-	else if (argc == 2)
+	else
 		system("leaks a.out && ./a.out");
 	ft_printf("Checked leaks in %s!\n", uname);
 }
 
 int	main(int argc, char **argv)
 {
-	check_leaks(argc, argv[1]);
+	if (argc == 2)
+		check_leaks(argc, argv[1]);
 	ft_printf("A%*.5s %*.5s$\n", -10, "123", -10, "4567");
 	printf("B%*.5s %*.5s$\n", -10, "123", -10, "4567");
 }
