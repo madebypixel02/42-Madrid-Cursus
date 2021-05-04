46ad5af8193f51f19ddb3fed8fc38fc8cf178e44
madebypixel02 <madebypixel02@gmail.com>
2021-05-04 11:25:29 +0200

    Added new function to make tests based on the OS :)

--- ft_printf/tests/main.c
+++ ft_printf/tests/main.c
@@ -6,27 +6,26 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 09:37:37 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/05/04 10:58:00 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/04 11:25:04 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 #include <limits.h>
 
+/* ******** Leaks ******** */
+void	check_leaks(int argc, char *uname)
+{
+	if (!ft_strncmp(uname, "Linux", 1) && argc == 1)
+		system("valgrind -q --leak-check=full ./a.out");
+	else if (argc == 1)
+		system("leaks a.out && ./a.out");
+	ft_printf("Checked leaks in %s!\n", uname);
+}
 
 int	main(int argc, char **argv)
 {
 	ft_printf("A%*.5s %*.5s\n", -10, "123", -10, "4567");
 	printf("B%*.5s %*.5s\n", -10, "123", -10, "4567");
-
-	/* ******** Leaks ******** */
-	char	*uname;
-
-	uname = (char *)popen("uname -s", "r");
-	if (!ft_strncmp(uname, "Linux", 5) && argc == 1)
-	{
-		system("valgrind -q --leak-check=full ./a.out 1 | cat -e");
+	check_leaks(argc, argv[1]);
 	}
-	else if (argc == 1)
-		system("leaks a.out && ./a.out 1 | cat -e");
-}
