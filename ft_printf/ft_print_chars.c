105909c3bc4aea15c6f053fc77718d40dcf9160a
Alejandro Perez bueno <aperez-b@c3r1s2.42madrid.com>
2021-05-08 13:41:55 +0200

    Fixed Zero flag in %c and %s

--- ft_printf/ft_print_chars.c
+++ ft_printf/ft_print_chars.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 10:43:07 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/05/01 09:50:44 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/08 10:35:19 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_print_c_pct(t_format f, va_list ap)
 {
 	char	c;
-	int		count;
+	int	count;
 
 	count = 0;
 	if (f.specifier == 'c')
@@ -23,8 +23,10 @@ int	ft_print_c_pct(t_format f, va_list ap)
 	else
 		c = '%';
 	f.precision = 1;
-	if (!f.minus && f.width - f.precision > 0)
-		count += ft_putnchar_fd(' ', 1, f.width - f.precision);
+	if (!f.minus && f.width > f.precision && !f.precision_specified && f.zero)
+		count += ft_putnchar_fd('0', 1, f.width - f.precision);
+ 	else if (!f.minus && f.width > f.precision)
+ 			count += ft_putnchar_fd(' ', 1, f.width - f.precision);
 	count += ft_putchar_fd(c, 1);
 	if (f.minus && f.width - f.precision > 0)
 		count += ft_putnchar_fd(' ', 1, f.width - f.precision);
@@ -34,18 +36,31 @@ int	ft_print_c_pct(t_format f, va_list ap)
 int	ft_print_s(t_format f, va_list ap)
 {
 	char	*string;
-	int		count;
-	int		len;
+	int	count;
+	int	len;
+	int	has_malloc;
 
 	count = 0;
+	has_malloc = 0;
 	string = va_arg(ap, char *);
+	if (!string)
+	{
+		string = malloc(sizeof(char) * 7);
+		has_malloc = 1;
+		ft_strlcpy(string, "(null)", 7);
+	}
 	len = ft_strlen(string);
-	if (!f.precision_specified)
+	if (!f.dot || f.precision > len || f.precision < 0)
 		f.precision = len;
-	if (!f.minus && f.width - f.precision > 0)
+	if (!f.minus && f.width > f.precision && !f.precision_specified && f.zero)
+		count += ft_putnchar_fd('0', 1, f.width - f.precision);
+	if (!f.minus && f.width - f.precision > 0 && !f.zero)
 		count += ft_putnchar_fd(' ', 1, f.width - f.precision);
 	count += ft_putstrn_fd(string, 1, f.precision);
 	if (f.minus && f.width - f.precision > 0)
 		count += ft_putnchar_fd(' ', 1, f.width - f.precision);
+	if (has_malloc)
+		free(string);
 	return (count);
 }
+
