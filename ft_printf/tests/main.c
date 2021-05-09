72b0e420e9260a813c76288233a1769e4ba36372
madebypixel02 <madebypixel02@gmail.com>
2021-05-09 21:49:28 +0200

    [BONUS] Initial take on the "space" flag

--- ft_printf/tests/main.c
+++ ft_printf/tests/main.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 09:37:37 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/05/09 17:31:30 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/09 21:46:15 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ int	main(int argc, char **argv)
 		check_leaks(argc, argv[1]);
 	else
 	{
-		printf("Len: %d\n", ft_printf("A%-2s, %.s, %-4s, %-2.4s, %-8.12s, %3s, %8s, %---2s, %.*s, %.0s, %.1s, %.2s, %.4s, %.8s", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, -2, NULL,     NULL, NULL, NULL, NULL, NULL));
-		printf("Len: %d\n", printf("B%-2s, %.s, %-4s, %-2.4s, %-8.12s, %3s, %8s, %---2s, %.*s, %.0s, %.1s, %.2s, %.4s, %.8s", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, -2, NULL,     NULL, NULL, NULL, NULL, NULL));
+		printf("Len: %d\n", ft_printf("A% -03d\n", 0));
+		printf("Len: %d\n", printf("B% -03d\n", 0));
+		//printf("Len: %d\n", printf("B%% *.5i 42 == |% *.5i|\n", 4, 42));
 	}
 }
