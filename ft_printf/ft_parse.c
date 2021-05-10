a09181e3154ed80e7cde5d3b77235ddb0b0659a1
Alejandro Perez bueno <aperez-b@c3r1s2.42madrid.com>
2021-05-10 15:20:29 +0200

    Fixed issue when multiple zeroes appear making width be 0

--- ft_printf/ft_parse.c
+++ ft_printf/ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 08:42:32 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/05/10 13:46:52 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/10 15:15:46 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static t_format	ft_parse_width(char *str, va_list	ap, t_format f)
 			f.minus = 1;
 		if (*str == '0' && !ft_isdigit(*(str - 1)))
 			f.zero = 1;
-		else if ((ft_isdigit(*str) || *str == '*') && !specified)
+		else if (((*str > '0' && *str <= '9') || *str == '*') && !specified)
 		{
 			if (*str == '*')
 				f.width = va_arg(ap, int);
