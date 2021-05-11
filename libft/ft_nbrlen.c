651bde6ed7c1a7bee98e2ceaecf7f3b4ad4b185b
Alejandro Perez bueno <aperez-b@c3r1s2.42madrid.com>
2021-05-11 19:08:27 +0200

    [BONUS] Some bug fixes

--- libft/ft_nbrlen.c
+++ libft/ft_nbrlen.c
@@ -6,11 +6,13 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 20:16:39 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/04/28 09:33:02 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/11 18:28:05 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_nbrlen(long n, int base)
+#include <stdio.h>
+
+int	ft_nbrlen(long long unsigned n, int base)
 {
 	int	len;
 
