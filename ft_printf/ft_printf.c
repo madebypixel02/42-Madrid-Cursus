a908a1094b37f4f5209bde885c63e0d75f425859
madebypixel02 <madebypixel02@gmail.com>
2021-05-13 18:11:58 +0200

    Fixed norminette

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 09:24:33 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/05/11 18:17:03 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/05/13 18:11:42 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,17 +44,17 @@ int	ft_print_format(t_format f, va_list ap)
 	if (!f.l_count && !f.h_count)
 	{
 		if (f.specifier == 'c' || f.specifier == '%')
-				count = ft_print_c_pct(f, ap);
+			count = ft_print_c_pct(f, ap);
 		if (f.specifier == 's')
-				count = ft_print_s(f, ap);
+			count = ft_print_s(f, ap);
 		if (f.specifier == 'd' || f.specifier == 'i' || f.specifier == 'u')
-				count = ft_print_d_i_u(f, ap);
+			count = ft_print_d_i_u(f, ap);
 		if (f.specifier == 'X' || f.specifier == 'x')
-				count = ft_print_x_X(f, ap);
+			count = ft_print_x_X(f, ap);
 		if (f.specifier == 'p')
-				count = ft_print_p(f, ap);
+			count = ft_print_p(f, ap);
 		if (f.specifier == 'f')
-				count = ft_print_f(f, ap);
+			count = ft_print_f(f, ap);
 	}
 	else
 		count = ft_print_bonus(f, ap);
@@ -73,7 +73,7 @@ int	ft_print_bonus(t_format f, va_list ap)
 		else if (f.specifier == 'x' || f.specifier == 'X')
 			count = ft_print_h_hex(f, ap);
 	}
-	else if(f.l_count)
+	else if (f.l_count)
 	{
 		if (f.specifier == 'd' || f.specifier == 'i' || f.specifier == 'u')
 			count = ft_print_l_diu(f, ap);
